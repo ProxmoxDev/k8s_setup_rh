2a5e4dcb725f0b44d08f34b5134048aa2c2ddd51
Syun Tanaka <42093052+piyoraik@users.noreply.github.com>
2024-05-16 21:22:01 +0900

    Update nfs_server_install.sh

--- nfs_server_install.sh
+++ nfs_server_install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 SHARE_DIRECTORY=/export/nfs
+PV_DIRECTORYS=50
 SHARE_ADDRESS=192.168.100.0/24
 
 dnf install -y nfs-utils-1:2.5.4-25.el9.x86_64
 mkdir -p ${SHARE_DIRECTORY}
 
-# i=1;i_max=100; while [ "$i" -le "$i_max" ]; do j=$(printf "%04d" $i); sudo mkdir /export/nfs/pv$j ; i=$(($i+1)) ; done
+i=1;i_max=$PV_DIRECTORYS; while [ "$i" -le "$i_max" ]; do j=$(printf "%04d" $i); sudo mkdir /export/nfs/pv$j ; i=$(($i+1)) ; done
 
 firewall-cmd --permanent --zone=public --add-service=nfs
 firewall-cmd --reload
