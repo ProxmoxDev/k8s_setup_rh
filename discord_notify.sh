2485f081684a2174d18cb1c923bc2c29c15c1b0e
Syun Tanaka <42093052+piyoraik@users.noreply.github.com>
2024-05-30 11:41:10 +0900

    Create discord_notify.sh

---
+++ discord_notify.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+DISCORD_WEBHOOK_URL=
+
+## Create Script
+mkdir discord-notify
+
+cat <<EOF | tee /root/discord-notify/start.sh
+#!/bin/bash
+
+discordWebhook=$DISCORD_WEBHOOK_URL
+
+curl=`cat <<EOS
+curl
+ --verbose
+ -X POST
+ ${discordWebhook}
+ -H 'Content-Type: application/json'
+ --data '{"content": "[XXXX] XXXX が起動しました"}'
+EOS`
+eval ${curl}
+EOF
+
+cat <<EOF | tee /root/discord-notify/stop.sh
+#!/bin/bash
+
+discordWebhook=$DISCORD_WEBHOOK_URL
+
+curl=`cat <<EOS
+curl
+ --verbose
+ -X POST
+ ${discordWebhook}
+ -H 'Content-Type: application/json'
+ --data '{"content": "[XXXX] XXXX を停止します"}'
+EOS`
+eval ${curl}
+EOF
+
+chmod +x /root/discord-notify/start.sh
+chmod +x /root/discord-notify/stop.sh
+
+## Create Service
+cat <<EOF | tee /etc/systemd/system/discord.service
+[Unit]
+Description=Start&stop Discord Notify service.
+After=syslog.target network-online.target
+
+[Service]
+User=root
+Type=oneshot
+RemainAfterExit=yes
+KillMode=none
+ExecStart=/bin/bash -c 'cd /root/discord-notify; ./start.sh'
+ExecStop=/bin/bash -c 'cd /root/discord-notify; ./stop.sh'
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable --now discord.service
