c8870a76c9111128d68040e402224c7cdf2d16f2
Syun Tanaka <42093052+piyoraik@users.noreply.github.com>
2024-06-19 16:03:22 +0900

    Create flannel_install.sh

---
+++ flannel_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## https://github.com/flannel-io/flannel
+kubectl create ns kube-flannel
+kubectl label --overwrite ns kube-flannel pod-security.kubernetes.io/enforce=privileged
+
+helm repo add flannel https://flannel-io.github.io/flannel/
+helm install flannel --set podCidr="10.244.0.0/16" --namespace kube-flannel flannel/flannel
